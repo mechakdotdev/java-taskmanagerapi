deeba61c3e94a3ff5edcbed52696e791c70f4d4d
Shak <88886274+mechakdotdev@users.noreply.github.com>
2024-09-26 12:49:39 +0100

    Add entity mappers (#10)

---
+++ src/main/java/taskmanager/mapper/TaskMapper.java
@@ -0,0 +1,34 @@
+package taskmanager.mapper;
+
+import taskmanager.dto.TaskDTO;
+import taskmanager.entity.Label;
+import taskmanager.entity.Project;
+import taskmanager.entity.Task;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TaskMapper {
+    public static TaskDTO toDto(Task task) {
+        return new TaskDTO(
+                task.getId(),
+                task.getProject().getId(),
+                task.getLabel().stream().map(Label::getId).collect(Collectors.toList()),
+                task.getPriority(),
+                task.getDueDate(),
+                task.getDescription()
+        );
+    }
+
+    public static Task toEntity(TaskDTO taskDTO, Project project, List<Label> labels) {
+        Task task = new Task(
+                project,
+                labels,
+                taskDTO.getPriority(),
+                taskDTO.getDueDate(),
+                taskDTO.getDescription()
+        );
+        task.setId(taskDTO.getId());
+        return task;
+    }
+}
